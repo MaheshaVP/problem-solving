19decc0680bf28b7ebd5ee71ffe53480c8d7f8ba
Mahesha V P <maheshavp12@gmail.com>
2026-03-24 17:09:34 +0530

    simple calculator logic

---
+++ calculator.py
@@ -0,0 +1,21 @@
+#calculator
+
+num1 = float(input("Enter num 1 : "))
+num2 = float(input("Enter num 2 : "))
+operator = input("Enter the operator (+,-,*,/) :")
+
+if operator == '+':
+    result = num1 + num2
+    print(round(result,3))
+elif operator == '-':
+    result = num1 - num2
+    print(round(result,3))
+elif operator == '*':
+    result = num1 * num2
+    print(round(result,3))
+elif operator == '/':
+    result = num1 / num2
+    print(round(result,3))
+else:
+    print(f"{operator} is not a valid operator")
+    print("Please enter a valid operator ")
